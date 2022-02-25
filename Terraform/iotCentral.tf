de8ded13e4422350a02fbd2c021fc2bee1a91df6
ViveLeFrance <joel.beimler@gmx.ch>
2022-02-25 12:37:39 +0100

    configured use of remote state within an az storage account

--- Terraform/iotCentral.tf
+++ Terraform/iotCentral.tf
@@ -6,6 +6,13 @@ terraform {
     }
   }
 
+  backend "azurerm" {
+      resource_group_name  = "rg-euwe-di-iotiac"
+      storage_account_name = "tfstate1a2b341"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
   required_version = ">= 1.1.0"
 }
 
